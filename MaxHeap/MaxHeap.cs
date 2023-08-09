cc122ee300960752fe5ed49c4c488ef2c9a7839f
Tiago Avila <tiago.avila09@gmail.com>
2023-08-09 11:32:05 -0300

    Updated summaries

--- MaxHeap/MaxHeap.cs
+++ MaxHeap/MaxHeap.cs
@@ -41,6 +41,16 @@ namespace Heap
             }
         }
 
+        /// <summary>
+        /// To Pop a value from the Max Heap we perform the following algorithm:
+        /// <list type="number">
+        ///     <item><description>Remove the greatest value, the one at index 0</description></item>
+        ///     <item><description>Move the value of the last available position in the Array to the position 0</description></item>
+        ///     <item><description>Then check if that Item has any children with greater value, if it has move the greatest child to its position</description></item>
+        ///     <item><description>Repeat the step above until reaches an Item that doesn't have children with a greater value or the end of the array</description></item>
+        /// </list>
+        /// </summary>
+        /// <returns></returns>
         public int Pop()
         {
             (int returnValue, _data[0], _data[_lastItemIndex]) = (_data[0], _data[_lastItemIndex], 0);
@@ -58,6 +68,22 @@ namespace Heap
             return returnValue;
         }
 
+        /// <summary>
+        /// Apply the Heap property to an unsorted array, in summary the algorithm checks every Node that has children (the parents),
+        /// checking if the children has greater value, swap if it has a greater value and then check the sub-trees.
+        /// Do that until checking all parents.
+        /// 
+        /// A detailed explanation is the following:
+        /// <list type="number">
+        ///     <item><description>Go through every Item that has children checking if any of them as a greater value</description></item>
+        ///     <item><description>If a children has a greater value, swap the child with largest value to the position of the parent 
+        ///     and recursivelly check the children of the child index that had the largest value, 
+        ///     this will apply the Heap property to all subtrees below the initial Parent</description></item>
+        ///     <item><description>Apply the step above until reaching a node that doesn't have children with greater values or have no children at all.</description></item>
+        ///     <item><description>After finishing the step above, move to the next item that has children</description></item>
+        /// </list>
+        /// </summary>
+        /// <param name="array"></param>
         public void Heapify(int[] array)
         {
             _lastItemIndex = array.Length - 1;
